4625340f91c5d0c54243183efab526877a2279da
satindar <satindar@gmail.com>
2015-02-26 10:37:01 -0800

    Initial commit

---
+++ spec/requests/user_creates_new_blog_post_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can create a new blog post' do
+  scenario 'Unregistered user visits new blog post page' do
+    visit root_path
+    click_link 'Add Post'
+
+    expect(page).to have_content('Create a post!')
+  end
+
+  scenario 'User creates a new post and it is persisted' do
+    visit root_path
+    click_link 'Add ride'
+    fill_in 'Title', with: 'Blarghh post'
+    fill_in 'Content', with: 'First post!'
+    click_button 'Submit Post'
+
+    within('#posts') do
+      expect(page).to have_content('First post!')
+    end
+  end
+end
